6f1f9940d8e5025f9625fa76583183d44750fa78
Nurzada <doulbekkyzyaizada@gmail.com>
2023-02-15 00:41:45 +0600

    todolist with redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
+
 import { useState } from "react";
 import "./App.css";
-import PostList from "./components/postList/PostList";
+import AddToDo from "./components/AddToDo/AddToDo";
+import Header from "./components/Header/Header";
+import ToDoList from "./components/ToDoList/ToDoList";
 
-const DUMMY_POSTS = [
-  { id: 1, title: "js", body: "description" },
-  { id: 2, title: "Next.js", body: "description 2" },
-  { id: 3, title: "React", body: "description 3" },
-];
+
 
 function App() {
-  const [posts, setPosts] = useState(DUMMY_POSTS);
+  
+
+
   return (
     <div className="App">
-      <div>
-        <PostList posts={posts} />
-      </div>
+
     </div>
   );
 }
